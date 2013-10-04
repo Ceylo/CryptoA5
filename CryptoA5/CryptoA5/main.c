11f38a2ec1487007754af1e3922d36c517edcbfd
Lucas Soltic <lucas.soltic@orange.fr>
2013-10-04 11:26:54 +0200

    Imported Xcode project

---
+++ CryptoA5/CryptoA5/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CryptoA5
+//
+//  Created by Ceylo on 04/10/13.
+//  Copyright (c) 2013 Yalir. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
