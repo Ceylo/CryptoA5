c23b3b37eb845ecc69c81c500c8f87d77385f556
Viewtiful <baptiste.haudegand@gmail.com>
2013-10-08 14:48:47 +0200

    Working on addition.

--- CryptoA5/CryptoA5/ec_operation_add.c
+++ CryptoA5/CryptoA5/ec_operation_add.c
@@ -8,7 +8,19 @@
 
 #include "ec_operations.h"
 
-PointRef PointCreateAdd(PointRef p, PointRef q)
+PointRef PointCreateAdd(PointRef p, PointRef q, mpz_t modulo)
 {
+    PointRef r = PointCreate();
+    
+    mpz_t lambda, lambda_rem, lambda_num, lambda_denum;
+    mpz_inits(lambda, lambda_num, lambda_denum, NULL);
+    
+    mpz_sub(lambda_num, p->y, q->y);
+    mpz_sub(lambda_denum, p->x, q->x);
+    
+    mpz_tdiv_qr(lambda, lambda_rem, lambda_num, lambda_denum);
+    
+    mpz_mod(lambda, lambda, modulo);
+    
 
 }
