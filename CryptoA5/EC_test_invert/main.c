c278cb3c81f6334c931b3e182eab1d7308f0a2cf
Viewtiful <baptiste.haudegand@gmail.com>
2013-10-10 11:46:27 +0200

    unit test for invert

---
+++ CryptoA5/EC_test_invert/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  EC_test_invert
+//
+//  Created by Baptiste on 10/10/13.
+//  Copyright (c) 2013 Yalir. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+int main(int argc, const char * argv[])
+{
+
+    
+    return 0;
+}
+
