b96879e0005d04a2376fdd618500b041a60ba44b
Viewtiful <baptiste.haudegand@gmail.com>
2013-10-10 11:07:44 +0200

    Changed name of the test for the addition to ‘EC_test_addition’

--- CryptoA5/EC_test_doublement/main.c
+++ CryptoA5/EC_test_doublement/main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  EC_tests
+//  EC_test_doublement
 //
 //  Created by Baptiste on 10/10/13.
 //  Copyright (c) 2013 Yalir. All rights reserved.
@@ -9,8 +9,6 @@
 #include <stdio.h>
 #include <gmp.h>
 #include "ec_operations.h"
-
-//(2,4) (3,1) a4 = 1
 
 int main(int argc, const char * argv[])
 {
@@ -21,10 +19,9 @@ int main(int argc, const char * argv[])
     
     PointRef g = PointCreateFromInt(0,1);
     PointRef p = PointCreateFromInt(2,4);
-    PointRef q = PointCreateFromInt(3,1);
     
     CurveRef curve = CurveCreate(mod, a, g);
-    PointRef r = PointCreateAdd(p, q, curve);
+    PointRef r = PointCreateAdd(p, p, curve);
     
     //do assert !
     gmp_printf("r = (%Zd, %Zd)\n", r->x, r->y);
@@ -32,9 +29,8 @@ int main(int argc, const char * argv[])
     mpz_clears(mod, a[0], a[1], a[2], a[3], a[4], a[5], a[6], NULL);
     PointDestroy(g);
     PointDestroy(p);
-    PointDestroy(q);
     PointDestroy(r);
-
+    
     return 0;
 }
 
