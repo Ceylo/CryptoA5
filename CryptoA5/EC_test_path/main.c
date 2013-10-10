9530ee174bf9c15f4e33a60164a86e85c8918f40
Lucas SOLTIC <lucas.soltic@orange.fr>
2013-10-10 11:27:16 +0200

    Better test for path utility

--- CryptoA5/EC_test_path/main.c
+++ CryptoA5/EC_test_path/main.c
@@ -8,16 +8,21 @@
 
 #include <stdio.h>
 #include "path.h"
+#include <string.h>
+#include <assert.h>
+
+#define DIR_PATH "/sdkjhf azfiubs df/zieduh"
+#define EXEC_NAME "aizuher"
+#define EXEC_PATH DIR_PATH "/" EXEC_NAME
+#define FILE_NAME "some curve.gp"
 
 int main(int argc, const char * argv[])
 {
 	char *filePath = NULL;
-	PathInit(argv[0]);
+	PathInit(EXEC_PATH);
+	PathForFile(FILE_NAME, &filePath);
 	
-	PathForFile("some curve.gp", &filePath);
-	
-	printf(">> %s\n", filePath);
-	
+	assert(strcmp(DIR_PATH "/" FILE_NAME, filePath) == 0);
     return 0;
 }
 
