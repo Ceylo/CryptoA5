d17f0ac33c4a3f2587a3cb168e37a1d0181313b1
Lucas SOLTIC <lucas.soltic@orange.fr>
2013-10-13 18:04:48 +0200

    Re-fix invert operation

--- CryptoA5/CryptoA5/ec_operation_invert.c
+++ CryptoA5/CryptoA5/ec_operation_invert.c
@@ -13,8 +13,28 @@ PointRef PointCreateInvert(PointRef p, CurveRef curve)
     PointRef q = PointCopy(p);
 	
 	//	P = (xp, yp)
-	// -P = (xp, yp + xp)
-	mpz_add(q->y, q->y, q->x);
+	// -P = (xp, - yp - a1.xp - a3)
+	
+	mpz_t mon;
+	
+	// mon = a1.xp
+	mpz_init_set(mon, curve->a[1]);
+	mpz_mul(mon, mon, q->x);
+	mpz_mod(mon, mon, curve->mod);
+	
+	// yp = -yp
+	mpz_neg(q->y, q->y);
+	mpz_mod(q->y, q->y, curve->mod);
+	
+	// yp = -yp - a1.xp
+	mpz_sub(q->y, q->y, mon);
+	mpz_mod(q->y, q->y, curve->mod);
+	
+	// yp = -yp - a1.xp - a3
+	mpz_sub(q->y, q->y, curve->a[3]);
+	mpz_mod(q->y, q->y, curve->mod);
+	
+	mpz_clear(mon);
 	
     return q;
 }
