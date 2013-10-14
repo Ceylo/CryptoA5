72e5ad6bf52057987ebced5c8fd2d0dac2a2d8ab
Viewtiful <baptiste.haudegand@gmail.com>
2013-10-14 18:14:33 +0200

    unit test for reading a file

--- CryptoA5/EC_test_readFile/main.c
+++ CryptoA5/EC_test_readFile/main.c
@@ -7,9 +7,19 @@
 //
 
 #include <stdio.h>
+#include "path.h"
+#include "ec_operations.h"
+#define FILE_NAME "w256-001.gp"
 
 int main(int argc, const char * argv[])
 {
+    
+    char *filePath = NULL;
+	PathInit(argv[0]);
+    PathForFile(FILE_NAME, &filePath);
+    
+    CurveCreateFromFile(filePath);
+    
 
     // insert code here...
     printf("Hello, World!\n");
