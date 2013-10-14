440800493d3a55af2adf1dc9035f1a9cd259f7b4
Viewtiful <baptiste.haudegand@gmail.com>
2013-10-14 17:44:00 +0200

    unit test for reading a file in input

---
+++ CryptoA5/EC_test_readFile/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  EC_test_readFile
+//
+//  Created by Baptiste on 14/10/13.
+//  Copyright (c) 2013 Yalir. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
