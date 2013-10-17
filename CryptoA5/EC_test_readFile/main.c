02049f3ee20a306e5af5b634b357e0b141d9dde6
Lucas SOLTIC <lucas.soltic@orange.fr>
2013-10-17 14:26:21 +0200

    Improved file reading test

--- CryptoA5/EC_test_readFile/main.c
+++ CryptoA5/EC_test_readFile/main.c
@@ -7,22 +7,30 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "path.h"
 #include "ec_operations.h"
 #define FILE_NAME "w256-001.gp"
 
 int main(int argc, const char * argv[])
 {
-    
     char *filePath = NULL;
 	PathInit(argv[0]);
     PathForFile(FILE_NAME, &filePath);
     
-    CurveCreateFromFile(filePath);
-    
-
-    // insert code here...
-    printf("Hello, World!\n");
+	assert(filePath != NULL);
+    CurveRef curve = CurveCreateFromFile(filePath);
+	
+	assert(curve != NULL);
+	assert(curve->a[4] != NULL);
+	assert(curve->a[6] != NULL);
+	assert(curve->g != NULL);
+	assert(curve->mod != NULL);
+	
+	CurveDestroy(curve);
+	free(filePath);
+	
     return 0;
 }
 
