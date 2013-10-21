e9e27e73356f0115fa5f2e3d1f186ba07c77d3c7
Viewtiful <baptiste.haudegand@gmail.com>
2013-10-21 22:08:10 +0200

    preparation of elgamal

---
+++ CryptoA5/ElGamal/client.h
@@ -0,0 +1,14 @@
+//
+//  client.h
+//  CryptoA5
+//
+//  Created by Baptiste on 21/10/13.
+//  Copyright (c) 2013 Yalir. All rights reserved.
+//
+
+#ifndef CryptoA5_client_h
+#define CryptoA5_client_h
+
+
+
+#endif
---
+++ CryptoA5/ElGamal/main.cpp
@@ -0,0 +1,60 @@
+//
+//  main.cpp
+//  ElGamal
+//
+//  Created by Baptiste on 21/10/13.
+//  Copyright (c) 2013 Yalir. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <SFML/Network.hpp>
+
+#include "ec_operations.h"
+#include "path.h"
+#include "client.h"
+#include "server.h"
+
+using namespace sf;
+using namespace std;
+
+void usage(const char *argv0)
+{
+	cout << "Usage: " << argv0 << " client | server" << endl;
+}
+
+void init(int argc, const char * argv[])
+{
+	srand((unsigned int)time(NULL));
+	PathInit(argv[0]);
+}
+
+int main(int argc, const char * argv[])
+{
+	if (argc < 2)
+	{
+		usage(argv[0]);
+		return 1;
+	}
+	
+	init(argc, argv);
+	string choice = argv[1];
+	
+	if (choice == "server")
+	{
+		server();
+	}
+	else if (choice == "client")
+	{
+		client();
+	}
+	else
+	{
+		usage(argv[0]);
+		return 1;
+	}
+	
+    return 0;
+}
+
+
