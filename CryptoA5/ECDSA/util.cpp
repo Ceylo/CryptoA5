b7e2ee71396be449b87d65763fdcfe590878c2f7
Lucas SOLTIC <lucas.soltic@orange.fr>
2013-10-22 11:27:38 +0200

    Modified sha256 for GMP

--- CryptoA5/ECDSA/util.cpp
+++ CryptoA5/ECDSA/util.cpp
@@ -66,17 +66,14 @@ string readRandomCurve()
 	return readFile(filename);
 }
 
-string sha256(const string& str)
+void sha256(mpz_t h, const string& str)
 {
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, str.c_str(), str.size());
     SHA256_Final(hash, &sha256);
-    stringstream ss;
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
-        ss << hex << setw(2) << setfill('0') << (int)hash[i];
-    }
-    return ss.str();
+	
+	mpz_init(h);
+	mpz_import (h, 256/8, 1, sizeof(hash[0]), 1, 0, hash);
 }
