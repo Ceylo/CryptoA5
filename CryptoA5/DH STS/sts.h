c9f9a2b2965696b629463af54e2658e62e398d69
Lucas SOLTIC <lucas.soltic@orange.fr>
2013-11-02 21:22:17 +0100

    Added comments on STSContext structure

--- CryptoA5/DH STS/sts.h
+++ CryptoA5/DH STS/sts.h
@@ -14,13 +14,12 @@
 using namespace sf;
 
 typedef struct {
-	TcpSocket stream;
-	
-	mpz_t dsaRand, dhRand;
-	CurveRef curve;
-	PointRef myDsaPubKey, myDhPubKey;
-	PointRef peerDsaPubKey, peerDhPubKey;
-	PointRef k;
+	TcpSocket stream;						// Network stream
+	mpz_t dsaRand, dhRand;					// Numbers for private keys
+	CurveRef curve;							// EC
+	PointRef myDsaPubKey, myDhPubKey;		// My public keys
+	PointRef peerDsaPubKey, peerDhPubKey;	// Peer public keys
+	PointRef k;								// AES secret key
 } STSContext;
 
 void sts_from_curve_maker(STSContext& context);
