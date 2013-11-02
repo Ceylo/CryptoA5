afb2108057ae3889e920532957e51f028da7336d
Baptiste <baptiste.haudegand@gmail.com>
2013-11-02 14:55:41 +0100

    Doing things like I think it's ok... that's it - void * returned for concatenation of mpz_t

--- CryptoA5/DH STS/util.h
+++ CryptoA5/DH STS/util.h
@@ -50,4 +50,4 @@ void * pointToKey(PointRef p);
 
 /** Concatenate 2 mpz_t and return it like a block of data
  */
-void* concatenateMpz_t(mpz_t u, mpz_t v);
+void* concatenateMpz_t(mpz_t u, mpz_t v, unsigned int* length);
