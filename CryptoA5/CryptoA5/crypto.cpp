55d37fc0f3a3c421998684ae3250335964d9e576
Lucas SOLTIC <lucas.soltic@orange.fr>
2013-11-03 22:32:21 +0100

    Factorized implementations

--- CryptoA5/CryptoA5/crypto.cpp
+++ CryptoA5/CryptoA5/crypto.cpp
@@ -128,64 +128,8 @@ bool dsa_receive_and_verify_message(TcpSocket& stream, CurveRef curve, PointRef 
 	mpz_init_set_str(u, uString.c_str(), 10);
 	mpz_init_set_str(v, vString.c_str(), 10);
 	
-	bool uvInBound = true;
-	uvInBound = uvInBound && mpz_cmp_si(u, 0) > 0;
-	uvInBound = uvInBound && mpz_cmp(u, curve->n) < 0;
-	uvInBound = uvInBound && mpz_cmp_si(v, 0) > 0;
-	uvInBound = uvInBound && mpz_cmp(v, curve->n) < 0;
-	
-	if (uvInBound)
-	{
-		PointRef p = curve->g;
-		PointRef q = pubKey;
-		PointRef p1 = NULL;
-		PointRef p2 = NULL;
-		PointRef xy = NULL;
-		
-		mpz_t hm, invV, lmon, rmon;
-		mpz_inits(hm, invV, lmon, rmon, NULL);
-		
-		// Compute left monomial: (H(m)/v mod n).P
-		sha256(hm, messageString.data(), messageString.size());
-		mpz_invert(invV, v, curve->n);
-		mpz_mul(lmon, hm, invV);
-		mpz_mod(lmon, lmon, curve->n);
-		p1 = PointCreateMultiple(p, lmon, curve);
-		
-		// Compute right monomial: (u/v mod n).Q
-		mpz_mul(rmon, u, invV);
-		mpz_mod(rmon, rmon, curve->n);
-		p2 = PointCreateMultiple(q, rmon, curve);
-		
-		xy = PointCreateAdd(p1, p2, curve);
-		
-		PointDestroy(p1), p1 = NULL;
-		PointDestroy(p2), p2 = NULL;
-		mpz_clears(hm, invV, lmon, rmon, NULL);
-		
-		if (mpz_cmp(u, xy->x) == 0)
-		{
-			PointRef nullPoint = PointCreateFromInt(0, 0);
-			
-			if (PointEqual(nullPoint, q) == false)
-			{
-				if (PointIsOnCurve(q, curve))
-				{
-					
-					PointRef m = PointCreateMultiple(q, curve->n, curve);
-					
-					if (PointEqual(m, nullPoint))
-					{
-						verified = true;
-					}
-					
-					PointDestroy(m), m = NULL;
-				}
-			}
-			
-			PointDestroy(nullPoint), nullPoint = NULL;
-		}
-	}
+	verified = dsa_verify_message(messageString.data(), messageString.size(), u, v, curve, pubKey);
+	mpz_clears(u, v);
 	
 	return verified;
 }
@@ -193,40 +137,7 @@ bool dsa_receive_and_verify_message(TcpSocket& stream, CurveRef curve, PointRef 
 void dsa_sign_and_send_message(TcpSocket& stream, const string& msg, mpz_t secret, CurveRef curve)
 {
 	mpz_t u, v;
-	
-    do {
-        mpz_t k;
-        secure_rand(k, curve->n);
-        PointRef p = curve->g;
-        PointRef kp = PointCreateMultiple(p, k, curve);
-        
-        mpz_init(u);
-        mpz_mod(u, kp->x, curve->n);
-        
-        mpz_t msgHashed;
-        mpz_inits(v, msgHashed, NULL);
-        
-        sha256(msgHashed, msg.data(), msg.size());
-        
-        mpz_mul(v, secret, u);
-        mpz_mod(v,v,curve->n);
-        
-        mpz_add(v, msgHashed, v);
-        mpz_mod(v,v, curve->n);
-        
-        mpz_t kinv;
-        mpz_init(kinv);
-        
-        mpz_invert(kinv, k, curve->n);
-        
-        mpz_mul(v, v, kinv);
-        mpz_mod(v, v, curve->n);
-        
-        mpz_clears(k, msgHashed, NULL);
-        PointDestroy(kp);
-		
-    } while ((mpz_cmp_ui(u, 0) == 0) && (mpz_cmp_ui(v, 0) == 0));
-	
+	dsa_sign_message(msg.data(), msg.size(), u, v, secret, curve);
     
     // Send u and v
 	char *uBuffer = NULL;
@@ -321,15 +232,15 @@ void dsa_sign_message(const void *data, size_t data_length, mpz_t& u, mpz_t& v, 
     
     do {
         mpz_t k;
+		mpz_t msgHashed;
+		mpz_t kinv;
+		
         secure_rand(k, curve->n);
         PointRef p = curve->g;
         PointRef kp = PointCreateMultiple(p, k, curve);
-        
-        mpz_init(u);
+		mpz_inits(u, v, msgHashed, kinv, NULL);
+		
         mpz_mod(u, kp->x, curve->n);
-        
-        mpz_t msgHashed;
-        mpz_inits(v, msgHashed, NULL);
         
         sha256(msgHashed, data, data_length);
         
@@ -339,13 +250,12 @@ void dsa_sign_message(const void *data, size_t data_length, mpz_t& u, mpz_t& v, 
         mpz_add(v, msgHashed, v);
         mpz_mod(v,v, curve->n);
         
-        mpz_t kinv;
-        mpz_init(kinv);
-        
         mpz_invert(kinv, k, curve->n);
         
         mpz_mul(v, v, kinv);
         mpz_mod(v, v, curve->n);
         
+		PointDestroy(kp);
+		mpz_clears(k, msgHashed, kinv, NULL);
     } while ((mpz_cmp_ui(u, 0) == 0) && (mpz_cmp_ui(v, 0) == 0));
 }
