2b72db5c3a6440e03f22384bdc648f95f2d75b76
Viewtiful <baptiste.haudegand@gmail.com>
2013-11-06 14:34:57 +0100

    Leak errors resolved for ElGamal

--- CryptoA5/ElGamal/server.cpp
+++ CryptoA5/ElGamal/server.cpp
@@ -26,32 +26,35 @@ void server()
 		return;
 	}
 	
-	// accept a new connection
-	sf::TcpSocket socket;
-	if (listener.accept(socket) != sf::Socket::Done)
-	{
-		perror("error when receiving new client");
-		return;
-	}
-    
-    string curveData = send_random_curve(socket);
-	
-	// Load curve
-	CurveRef curve = CurveCreateFromData(curveData.c_str());
-	mpz_t a;
-    
-    PointRef q = create_key(curve, a);
-    
-    send_key(socket, q);
-	
-	string msg = eg_receive_and_decrypt_message(socket, a, curve);
-    
-	cout << "Received: " << msg << endl;
-	
-    mpz_clear(a);
-    CurveDestroy(curve);
-    PointDestroy(q);
-    
-    socket.disconnect();
+    while(1)
+    {
+        // accept a new connection
+        sf::TcpSocket socket;
+        if (listener.accept(socket) != sf::Socket::Done)
+        {
+            perror("error when receiving new client");
+            return;
+        }
+        
+        string curveData = send_random_curve(socket);
+        
+        // Load curve
+        CurveRef curve = CurveCreateFromData(curveData.c_str());
+        mpz_t a;
+        
+        PointRef q = create_key(curve, a);
+        
+        send_key(socket, q);
+        
+        string msg = eg_receive_and_decrypt_message(socket, a, curve);
+        
+        cout << "Received: " << msg << endl;
+        
+        mpz_clear(a);
+        CurveDestroy(curve);
+        PointDestroy(q);
+        
+        socket.disconnect();
+    }
 	listener.close();
 }
